f61e2419b565139c6e235a6c23db0c046aa1e43f
ctres <treseler@gmail.com>
2013-08-21 10:13:05 +0200

    initiating rocket_pants

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,9 @@
 class Job < ActiveRecord::Base
   #Associations
   belongs_to :user
-  has_one :item
+  has_many :items
   has_many :addresses
+
 
   #Attributes
   attr_accessible :name, :price, :quantity, :status, :tracking
