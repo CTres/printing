9b151ac59223c01ff09b805ea169e56a75e281af
ctres <treseler@gmail.com>
2013-09-05 17:22:36 +0200

    iframe updated, seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.create!(email: "treseler@gmail.com", password: "password")
+user.templates.create!(name: "invoice", url: "https://docs.google.com/document/d/1xil2lc4CHNlfwt0eWcYqWfVZEm4IAgS0DxjeTHQ7S_0/edit?embedded=true")
+user.templates.create!(name: "account agreement")
+user.templates.create!(name: "indemnification agreement")
+user.templates.create!(name: "confidentiality agreement")
+user.templates.create!(name: "monthly sendout")
+user.templates.create!(name: "christmas campaign")
